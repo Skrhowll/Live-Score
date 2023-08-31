dd7b7b98a5ce23289fe5dcbc1dc4daa73016fa51
Clément <clement.chicot29@gmail.com>
2023-08-31 12:10:55 +0200

    ajout de la page front faite par leo et adaptation du code

--- README.md
+++
@@ -1,2 +0,0 @@
-# Live-Score
-School TP
--- refresh_score.js
+++ refresh_score.js
@@ -1,21 +1,18 @@
-const matchList = document.getElementById("matchList");
+const scoreEquipe1 = document.getElementById("equipe1");
+const scoreEquipe2 = document.getElementById("equipe2");
+
 
 // Fonction pour récuperer et actualiser les scores
 async function fetchLiveScores() {
+ 
   try {
     const res = await fetch("matches.json"); // API Qui renvoie le JSON des scores
+    
     const data = await res.json();
-
-    matchList.innerHTML = "";
-
+    
     for (const match of data) {
-      const matchDiv = document.createElement("div");
-      matchDiv.className = "match";
-      matchDiv.innerHTML = `
-      <h2>${match.equipe1} vs ${match.equipe2}</h2>
-      <p>Score: ${match.score1} - ${match.score2}</p>
-      `;
-      matchList.appendChild(matchDiv);
+      scoreEquipe1.innerHTML = `${match.score1}`;
+      scoreEquipe2.innerHTML = `${match.score2}`;
     }
   } catch (error) {
     console.error("Erreur en rappatriant les scores en direct", error);
