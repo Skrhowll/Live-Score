830328501df16d593dffdbf4a983cef50e456c76
Skrhowll <103193056+Skrhowll@users.noreply.github.com>
2023-09-08 09:47:24 +0200

    Add files via upload

--- api/AdminToken.php
+++ api/AdminToken.php
@@ -5,22 +5,26 @@
 </head>
 <body>
     <h1>Générateur de Token</h1>
-    <form id="tokenForm" method="post">
+
+    <form id="tokenForm" action="traitement.php" method="post">
         <p>Entrez votre adresse e-mail :</p>
         <input type="email" name="email" id="email" placeholder="Adresse e-mail" required>
         <input type="submit" name="submit" value="Générer Token">
     </form>
+
     <p>Token généré :</p>
     <p id="tokenDisplay">
         <?php
         // Afficher le token généré après soumission du formulaire, si disponible
+        session_start();
         if (isset($_SESSION['token'])) {
             echo $_SESSION['token'];
             unset($_SESSION['token']); // Effacer la variable de session après affichage
         }
+        session_write_close();
         ?>
     </p>
-
-    <script src="generateToken.js"></script>
 </body>
 </html>
+
+
--- api/traitement.php
+++ api/traitement.php
@@ -1,19 +1,35 @@
 <?php
-    // Générer un token
-    $heure_actuelle = date('Hi');
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomPart = '';
+// Fonction pour générer un token
+function generateToken() {
+    // Obtenez l'heure actuelle au format Unix timestamp
+    $timestamp = time();
 
-    // Générer 12 caractères aléatoires
-    for ($i = 0; $i < 12; $i++) {
-        $randomPart .= $characters[rand(0, $charactersLength - 1)];
+    // Caractères disponibles pour le token
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    // Longueur du token souhaitée
+    $token_length = 32; // Vous pouvez ajuster cette longueur selon vos besoins
+
+    // Initialisation du token avec le timestamp
+    $token = (string)$timestamp;
+
+    // Ajoutez des caractères aléatoires pour compléter la longueur du token
+    $characters_length = strlen($characters);
+    for ($i = strlen($token); $i < $token_length; $i++) {
+        $random_character = $characters[rand(0, $characters_length - 1)];
+        $token .= $random_character;
     }
 
-    // Concaténer l'heure avec les caractères aléatoires
-    $token = $heure_actuelle . $randomPart;
+    return $token;
+}
 
-    $sql = 'mysql';
+// Générez un token
+$token = generateToken();
+
+// Affichez le token
+echo $token;
+
+    /*$sql = 'mysql';
     $host = 'localhost';
     $bdd = 'Live_Score';
     $login = 'root';
@@ -31,40 +47,23 @@
         $ins = $cnx->prepare("INSERT INTO Live_Score.user (mail, cle) VALUES(?, ?)");
         $ins->execute(["jonas.pustay@outlook.fr", $token]);
 
-        // Affichez un message de succès
-        echo "Adresse e-mail insérée avec succès dans la base de données : $email";
-
         // Fermez la connexion à la base de données
         $cnx = null;
-    } catch (PDOException $e) {
-        echo "Erreur lors de l'insertion dans la base de données : " . $e->getMessage();
-    }
-    // Récupérer l'adresse e-mail depuis la requête GET
-$mail = isset($_GET['email']) ? $_GET['email'] : '';
+    } catch (PDOException $e) {}*/
 
-// Adresse e-mail de l'expéditeur
-$expediteur = "wilfriedmasoua@yahoo.fr";
+    // Vérifie si le formulaire a été soumis
+if (isset($_POST['submit'])) {
+    // Génère un token
+    $generated_token = generateToken();
 
-// Vérifier si l'adresse e-mail du destinataire n'est pas vide
-if (!empty($mail)) {
-    // Envoyer un e-mail avec le token
-    $destinataire = $mail; // Adresse e-mail du destinataire
+    // Stocke le token dans une variable de session pour l'afficher sur la page principale
+    session_start();
+    $_SESSION['token'] = $generated_token;
+    session_write_close();
 
-    $subject = "Votre token généré";
-    $message = "Voici votre token généré : " . $token;
-
-    // En-têtes de l'e-mail
-    $headers = "From: $expediteur\r\n"; // Expéditeur
-    $headers .= "Reply-To: $expediteur\r\n"; // Répondre à l'expéditeur
-
-    if (mail($destinataire, $subject, $message, $headers)) {
-        echo 'Token généré et envoyé par e-mail à ' . $destinataire;
-    } else {
-        echo 'Erreur lors de l'envoi de l'e-mail.';
-    }
-} else 
-{
-    echo "L'adresse e-mail est vide. Veuillez saisir une adresse e-mail.";
+    // Redirige vers la page principale pour afficher le token
+    header('Location: AdminToken.php');
+    exit();
 }
-?>
 
+?>
